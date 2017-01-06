40a8a42d137ae13eb1ea9bb2d7515f933b2c28af
Wes Ferrer <wesferrer@gmail.copm>
2017-01-06 13:15:59 -0800

    requirements done

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,7 +19,7 @@ class TweetsController < ApplicationController
   end
 
   def edit
-
+    @tweet = Tweet.find(params[:id])
   end
 
   def show
@@ -27,11 +27,19 @@ class TweetsController < ApplicationController
   end
 
   def update
+    @tweet = Tweet.find(params[:id])
 
+    if @tweet.update_attributes(params.require(:tweet).permit(:handle, :content))
+      redirect_to tweets_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to tweets_path
   end
 
 
